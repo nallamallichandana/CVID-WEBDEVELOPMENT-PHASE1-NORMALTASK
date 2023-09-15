d4569e011f75c0b501b44fe2c2dcb3a354958a88
nallamallichandana <143244081+nallamallichandana@users.noreply.github.com>
2023-09-15 20:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add interactivity here (e.g., handling clicks, displaying content)
+
+// Example: When a movie card is clicked, open a details page or play the video.
+document.querySelectorAll('.movie-card').forEach(card => {
+    card.addEventListener('click', () => {
+        // Implement your logic here
+    });
+});
